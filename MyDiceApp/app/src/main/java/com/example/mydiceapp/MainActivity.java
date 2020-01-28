4a65c39b22ae9fa736574f3010555e8e960bd2fc
Omer Quadri M <omar@DSmart.com>
2020-01-28 15:57:35 +0530

    Added Dice App

--- MyDiceApp/app/src/main/java/com/example/mydiceapp/MainActivity.java
+++ MyDiceApp/app/src/main/java/com/example/mydiceapp/MainActivity.java
@@ -10,40 +10,53 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_main);
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        final ImageView diceImg1 = findViewById (R.id.imgDice1);
-        final ImageView diceImg2 = findViewById (R.id.imgDice2);
+        final ImageView diceImg1 = findViewById(R.id.imgDice1);
+        final ImageView diceImg2 = findViewById(R.id.imgDice2);
 
         final int[] DiceArray = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6,};
 
-        Button btnRoll = findViewById (R.id.btnRoll);
-        final MediaPlayer mp = MediaPlayer.create (this, R.raw.dice_sound);
+        Button btnRoll = findViewById(R.id.btnRoll);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.dice_sound);
 
-        btnRoll.setOnClickListener (new View.OnClickListener () {
+        btnRoll.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick (View v) {
 
                 /*Creating a Random class Object to generate Random Numbers*/
 
-                Random rndObj = new Random ();
-                int myRandomNumber = rndObj.nextInt (6); // Bound gives random number between 0 .. 5
+                Random rndObj = new Random();
+                int myRandomNumber = rndObj.nextInt(6); // Bound gives random number between 0 .. 5
 
                 //Log.i ("DiceRoller", "The generated Random Number is " + myRandomNumber);
 
-                diceImg1.setImageResource (DiceArray[myRandomNumber]);
+                diceImg1.setImageResource(DiceArray[ myRandomNumber ]);
 
-                myRandomNumber = rndObj.nextInt (6);
-                diceImg2.setImageResource (DiceArray[myRandomNumber]);
+                myRandomNumber = rndObj.nextInt(6);
+                diceImg2.setImageResource(DiceArray[ myRandomNumber ]);
 
-                mp.start ();
+                YoYo.with(Techniques.Shake)
+                        .duration(700)
+                        .repeat(0)
+                        .playOn(diceImg1);
+
+                YoYo.with(Techniques.Shake)
+                        .duration(700)
+                        .repeat(0)
+                        .playOn(diceImg2);
+
+                mp.start();
             }
         });
     }
